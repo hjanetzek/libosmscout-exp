4f4c9bd4ad91c12414ee0c3648a85105cb74dfca
Hannes Janetzek <hannes.janetzek@gmail.com>
2014-09-10 19:33:07 +0200

    wip: normalize utf-8 in TextSearchIndex

--- libosmscout/src/osmscout/TextSearchIndex.cpp
+++ libosmscout/src/osmscout/TextSearchIndex.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <osmscout/util/String.h>
 #include <osmscout/TextSearchIndex.h>
+#include <osmscout/util/utf8.h>
 
 namespace osmscout
 {
@@ -101,7 +102,7 @@ namespace osmscout
   }
 
 
-  bool TextSearchIndex::Search(const std::string& query,
+  bool TextSearchIndex::Search(const std::string& querystring,
                                bool searchPOIs,
                                bool searchLocations,
                                bool searchRegions,
@@ -110,9 +111,11 @@ namespace osmscout
   {
     results.clear();
 
-    if(query.empty()) {
+    if(querystring.empty()) {
       return true;
     }
+
+    std::string query = Utf8Util::StringToAscii(querystring, true);
 
     std::vector<bool> searchGroups;
 
@@ -182,6 +185,8 @@ namespace osmscout
 
       idx--;
     }
+
+    //offset -= 1;
 
     // Immediately preceding the FileOffset is
     // a single byte that denotes offset type
